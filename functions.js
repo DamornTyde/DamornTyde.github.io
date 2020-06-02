024363be1d116fcb427afe59b10fa0b7af234a6e
Ronald van Zon <34279265+DamornTyde@users.noreply.github.com>
2020-06-02 14:02:55 +0200

    update

--- functions.js
+++ functions.js
@@ -1,6 +1,34 @@
-fetch(functions.php)
+fetch("https://api.github.com/users/DamornTyde/repos")
 .then(response => response.json())
 .then(data => {
-    console.log(data);
+    data.forEach(item => {
+        if (item.name != "damorntyde.github.io") {
+            const temp = document.createDocumentFragment();
+            const header = document.createElement("h2");
+            const descr = document.createElement("p");
+            header.appendChild(document.createTextNode(item.name));
+            temp.appendChild(header);
+            descr.appendChild(document.createTextNode(item.description));
+            temp.appendChild(descr);
+            temp.appendChild(createButton("code", () => code(item.name)));
+            temp.appendChild(createButton("showcase", () => showcase(item.name)));
+            document.querySelector("main").appendChild(temp);
+        }
+    });
 })
 .catch(error => console.error(error));
+
+function createButton(text, onClicked) {
+    const button = document.createElement("button");
+    button.appendChild(document.createTextNode(text));
+    button.addEventListener("click", onClicked);
+    return button;
+}
+
+function code(item) {
+    window.open(`https://www.github.com/DamornTyde/${item}`);
+}
+
+function showcase(item) {
+    window.open(`https://damorntyde.github.io/${item}`);
+}
