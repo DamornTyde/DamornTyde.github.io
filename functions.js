31a35b45548880dc14871fe5a17330a18be6f4b9
Ronald van Zon <34279265+DamornTyde@users.noreply.github.com>
2020-09-10 11:12:38 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -12,13 +12,19 @@ fetch("https://api.github.com/users/DamornTyde/repos")
             descr.appendChild(document.createTextNode(item.description));
             temp.appendChild(descr);
             buttons.appendChild(createButton("code", `https://www.github.com/DamornTyde/${item.name}`));
-            buttons.appendChild(createButton("showcase", `https://damorntyde.github.io/${item.name}`));
+            if (item.has_pages) {
+                buttons.appendChild(createButton("showcase", `https://damorntyde.github.io/${item.name}`));
+            }
             temp.appendChild(buttons);
             document.querySelector("main").appendChild(temp);
         }
     });
 })
-.catch(error => console.error(error));
+.catch(error => {
+    const image = document.createElement("img");
+    image.setAtribute("src", `https://http.cat/${error.status}.jpg`);
+    document.querySelector("main").appendChild(image);
+});
 
 function createButton(text, onClicked) {
     const button = document.createElement("button");
